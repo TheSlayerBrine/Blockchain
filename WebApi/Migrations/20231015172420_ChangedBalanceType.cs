75ac8c014aa8c74729f814d9fa2f6f89754372af
TheSlayerBrine <cgamer788@gmail.com>
2023-10-15 20:28:14 +0300

    deleted files?

---
+++ WebApi/Migrations/20231015172420_ChangedBalanceType.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blockchain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedBalanceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SuppySold",
+                table: "SmartContracts",
+                newName: "SupplySold");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AmountExchanged",
+                table: "TransactionPurchases",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "SmartContracts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Balance",
+                table: "Accounts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SupplySold",
+                table: "SmartContracts",
+                newName: "SuppySold");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "AmountExchanged",
+                table: "TransactionPurchases",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "SmartContracts",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Balance",
+                table: "Accounts",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
